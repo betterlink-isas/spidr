e10c1cc0cf12eb297be1e0261607b34237ab0c74
Dak Smyth <tpzker@thepuzzlemaker.info>
2019-12-20 18:15:33 -0600

    Add ls|list, help commands, allow listing of domains with authority over

--- backend/backend.js
+++ backend/backend.js
@@ -9,6 +9,7 @@ function returnObject(socket, data) {
 var tokens = {}
 
 var server = new net.createServer(function(socket) {
+    //console.log("CONN " + socket.remoteAddress);
     socket.setNoDelay(true);
     socket.on('data', function(data) {
         try {
@@ -34,6 +35,12 @@ var server = new net.createServer(function(socket) {
                                     "token": uuid
                                 });
                                 console.log("AUTH " + user);
+                            } else {
+                                returnObject(socket, {
+                                    "type": "AUTH RESPONSE",
+                                    "status": "BAD AUTH"
+                                });
+                                console.log("FAIL AUTH (BC.) " + user);
                             }
                         } catch (err) {
                             console.error(err);
@@ -47,6 +54,7 @@ var server = new net.createServer(function(socket) {
                             "type": "AUTH RESPONSE",
                             "status": "BAD AUTH"
                         });
+                        console.log("FAIL AUTH (NE.) " + user);
                     }
                 }
             } else if (json.type == "AUTH DEAUTH") {
@@ -58,12 +66,51 @@ var server = new net.createServer(function(socket) {
                     tokens[token] = null;
                 }
                 returnObject(socket, {"type": "AUTH RESPONSE", "status": "GOOD DEAUTH"})
+            } else if (json.type == "DOMAIN LIST") {
+                var token = json.token;
+                if (token == null) {
+                    returnObject(socket, {"type": "ERROR", "error": "Malformed request (token not present)"});
+                } else {
+                    if (fs.existsSync('authorities/' + tokens[token] + '.json')) {
+                        var data = fs.readFileSync('authorities/' + tokens[token] + '.json');
+                        try {
+                            var user = JSON.parse(data);
+                            var domains = user.domains;
+                            if (domains == null) {
+                                returnObject(socket, {
+                                    "type": "DOMAIN RESPONSE",
+                                    "status": "BAD USERFILE"
+                                });
+                            } else {
+                                returnObject(socket, {
+                                    "type": "DOMAIN RESPONSE",
+                                    "status": "GOOD LIST",
+                                    "list": domains
+                                });
+                            }
+                        } catch (err) {
+                            console.error(err);
+                            returnObject(socket, {
+                                "type": "DOMAIN RESPONSE",
+                                "status": "BAD USERFILE"
+                            });
+                        }
+                    } else {
+                        returnObject(socket, {
+                            "type": "DOMAIN RESPONSE",
+                            "status": "BAD TOKEN"
+                        });
+                    }
+                }
             } else if (json.type == null) {
                 returnObject(socket, {"type": "ERROR", "error": "Malformed request (type not present)"});
             }
         } catch (err) {
             returnObject(socket, {"type": "ERROR", "error": err.toString()});
         }
+    });
+    socket.on('close', function() {
+        //console.log("DECONN " + socket.remoteAddress);
     });
 });
 
--- frontend/shell/dpsh.lua
+++ frontend/shell/dpsh.lua
@@ -14,7 +14,6 @@ if ops.h or ops.help then
   os.exit(0)
 end
 term.write("Username: ")
-math.large = 9223372036854775807
 local user = term.read()
 if user == false or user == nil then
   os.exit(0)
@@ -49,6 +48,11 @@ if ops.p or ops.printHash then
   os.exit(0)
 end
 local socket = internet.open("68.102.163.235", 8053)
+if (socket) then
+else
+  error("Could not connect to server")
+end
+socket:setTimeout(5)
 socket:setvbuf("no")
 socket:write(json.encode(authRequest))
 local data = json.decode(socket:read("*l"))
@@ -76,14 +80,70 @@ else
   error("Invalid response from server")
 end
 
+function splitBySpaces(string)
+  chunks = {}
+  for substring in string:gmatch("%S+") do
+    table.insert(chunks, substring)
+  end
+  return chunks
+end
+
+hist = {}
 while true do
   term.write("dpsh$ ")
-  local command = term.read()
+  local command = term.read(hist)
   if command == false or command == nil or command == "exit\n" or command == "logout\n" then
     break
   end
   command = command:sub(0, #command-1)
-  print(command)
+  command = command:lower()
+  hist[#hist+1] = command
+  args = splitBySpaces(command)
+  command = args[1]
+  if command == "ls" or command == "list" then
+    socket:write(json.encode({["type"]="DOMAIN LIST",["token"]=token}))
+    local data = json.decode(socket:read("*l"))
+    if data.type == "DOMAIN RESPONSE" then
+      if data.status == "BAD USERFILE" then
+        print("Bad user file, please contact administrator")
+      elseif data.status == "BAD TOKEN" then
+        print("Bad token, please reauth and try again (close and reopen dpsh)")
+      elseif data.status == "GOOD LIST" then
+        if data.list then
+          for _,v in ipairs(data.list) do
+            print(v)
+          end
+        else
+          error("Invalid response from server")
+        end
+      else
+        error("Invalid response from server")
+      end
+    elseif data.type == "ERROR" then
+      if data.error then
+        error(data.error)
+      else
+        error("Invalid response from server")
+      end
+    else
+      error("Invalid response from server")
+    end
+  elseif command == "help" then
+    if #args == 1 then
+      print("Commands: help, ls|list, exit|logout")
+    else
+      comm = args[2]
+      if comm == "list" or comm == "ls" then
+        print("ls|list: lists all domains you have authority over")
+      elseif comm == "exit" or comm == "logout" then
+        print("exit|logout: deauths and closes the console. Same effect with ^D and ^C")
+      else
+        print("Invalid command!")
+      end
+    end
+  else
+    print("Invalid command!")
+  end
 end
 socket:write(json.encode({
   ["type"] = "AUTH DEAUTH",
